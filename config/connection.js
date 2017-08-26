92752dd19cabb7f204d2932c49390b2e2d510f0c
Jim Gray <JimGray9999@users.noreply.github.com>
2017-08-25 22:36:47 -0400

    updates

--- config/connection.js
+++ config/connection.js
@@ -14,5 +14,13 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-connection.connect();
+// Initiate MySQL Connection
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
