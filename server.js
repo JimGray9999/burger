e4a1edcada347974f794c2db0f3d3659fe38202b
Jim Gray <JimGray9999@users.noreply.github.com>
2017-08-26 23:00:17 -0400

    updates

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ app.set("view engine", "handlebars");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(methodOverride("_method")); 
+app.use(methodOverride("_method"));
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(bodyParser.text());
 app.use(bodyParser.json({
   type: "application/vnd.api+json"
