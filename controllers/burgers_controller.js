ce4db1a91a2886878b21a38f5a8e94b940b53d55
Jim Gray <JimGray9999@users.noreply.github.com>
2017-08-31 21:34:12 -0400

    updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,16 +5,16 @@ var router = express.Router();
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-  burger.read(function(data) {
+router.get("/", function (req, res) {
+  burger.read(function (data) {
     var brgObject = {
       burgers: data
     };
   })
 });
 
-router.post("/", function(req, res) {
-  burger.create(function(data) {
+router.post("/", function (req, res) {
+  burger.create(function (data) {
     
   })
 });
--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,13 @@
 var orm = require ("../config/orm.js");
 
 var burger = {
-  read: function( cb) {
+  read: function(cb) {
     orm.selectAll("burgers", function (res) {
       cb(res);
     });
   },
   create: function (cb) {
-    orm.insertOne("burgers", function (res) {
+    orm.insertOne("burgers", col, vals, function (res) {
       cb(res);
     })
   }, 
