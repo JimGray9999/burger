55f554333c0d3fdf4a35a4f457f910a1f8107223
Jim Gray <jimgray2005@gmail.com>
2020-01-14 16:57:51 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'iamroot123',
     database: 'burgers_db'
   });
 };
