fbd2d2370f08ebde2900231754f63487a2d9ab30
Jim Gray <jimgray2005@gmail.com>
2020-02-16 10:52:44 -0500

    upgrading to a PWA

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,9 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
-    password: 'password',
+    password: 'iamroot123',
     database: 'burgers_db'
   });
 };
